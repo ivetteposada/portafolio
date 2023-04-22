097a72fcd386daf0bcc64d67f649b316c98aefc0
ivetteposada <ivette.posada@gmail.com>
2023-04-22 13:23:01 -0600

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import { NavBar } from './components/NavBar';
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import {Sidebar} from './components/Sidebar';
+import { Container } from 'react-bootstrap';
+import TypingEffect from './components/TypingEffect';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div className="App">   
+                  
+        <Sidebar />
+        <NavBar/>            
+        <Routes>  
+            <Route path="/" element={<Home/>} />                                   
+            <Route path="/skills" element={<Skills/>} />                          
+            <Route path="/projects" element={<Projects/>} /> 
+        </Routes>         
+                
     </div>
-  );
+    );
 }
-
-export default App;
+    
+    export default App;
+    
